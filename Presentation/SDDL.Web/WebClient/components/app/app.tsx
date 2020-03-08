cf53683d8716170e54f181747553640afe2101da
Simone Bembi <simone.bembi@gmail.com>
2020-03-08 19:27:53 +0100

    Extended graphical boilerplate

--- Presentation/SDDL.Web/WebClient/components/app/app.tsx
+++ Presentation/SDDL.Web/WebClient/components/app/app.tsx
@@ -1,33 +1,9 @@
 import * as React from 'react';
 import { Hello } from '../world/world';
 import './app.scss';
+import { DownloadsContainer } from '../downloads/downloads-container';
 
-export const App = () => {
-
-    const [active, setActive] = React.useState(false);
-
-    return <div className="app__component">
-        <div className="downloads-container__component">
-            <div className={`download ${active ? '--active' : ''}`} onClick={() => setActive(!active)}>
-                <div className="__column">
-                    <div className="__column-header">URL</div>
-                    <div className="__column-content">https://hello.warudo</div>
-                </div>
-                <div className="__column">
-                    <div className="__column-header">Status</div>
-                    <div className="__column-content">Recording</div>
-                </div>
-            </div>
-            <div className="download">
-                <div className="__column">
-                    <div className="__column-header">URL</div>
-                    <div className="__column-content">https://hello.warudo</div>
-                </div>
-                <div className="__column">
-                    <div className="__column-header">Status</div>
-                    <div className="__column-content">Recording</div>
-                </div>
-            </div>
-        </div>
-    </div>;
-}
+export const App = () => <div className="app__component">
+    <h1 className="__header">Downloads</h1>
+    <DownloadsContainer />
+</div>;
